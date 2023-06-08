9c3ad87677ce4a488af2c92e285adc05bb7b936f
elena keller <ek091409@icloud.com>
2023-06-08 13:23:36 -0500

    Initial Commit

---
+++ about me/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  about me
+//
+//  Created by Scholar on 6/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text ("about elena"){
+                Button("about me") {
+                }
+                Image("pancakes copy")
+                    .resizable(resizingMode:.stretch)
+                    .aspectRatio(contentMode:.fit)
+            }
+            .font(.title2)
+            
+            
+        }
+        
+        
+        struct ContentView_Previews:
+            PreviewProvider {
+            static var previews: some View {
+                ContentView()
+            }
+        }
+        
+    }
+}
---
+++ about me/about_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  about_meApp.swift
+//  about me
+//
+//  Created by Scholar on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct about_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
